eb233b873c72a6f4791480cc276f08058ed213f4
wellingtonpombo <wpombosousa@gmail.com>
2014-11-22 19:20:29 -0200

    teste
    teste

---
+++ scripts/mycode.js
@@ -0,0 +1,26 @@
+﻿var app = angular.module('HackingCodeApp', []);
+
+
+app.controller('MainController', function ($rootScope, $http, $scope) {
+
+    $scope.nome = "";
+    $scope.Objeto = [];
+    $scope.Objeto.nome = "";
+    $scope.Objeto.status = "";
+    $scope.Objeto.numero = "";
+
+    $scope.registro = function () {    
+        $http.post('http://hackingcode.com.br:9898/empresa').success(RegistroCallback);
+    }
+    
+    function RegistroCallback(data, status) {        
+        $scope.lista = data;
+    }
+
+
+    
+});
+
+
+
+
